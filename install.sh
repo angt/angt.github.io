198bdf3e09b6da6682a39edf0ae5755259c1212b
Adrien Gallouët <adrien@gallouet.fr>
2021-01-13 16:41:05 +0100

    Add install.sh
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+cd "$HOME"
+
+remote() {
+	exec 2>/dev/null >/dev/null
+	git remote add "$1" "$2" || true
+	git remote set-url "$1" "$2"
+	git remote set-url --push "$1" "$3"
+}
+
+git init
+
+remote origin https://github.com/angt/dotfiles gh:angt/dotfiles
+remote gitlab https://gitlab.com/angt/dotfiles gl:angt/dotfiles
+
+git fetch --all
+git reset origin/master
