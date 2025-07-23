cb8649a9a23828a51128203e0e54e8db39b468a1
Adrien Gallouët <adrien@gallouet.fr>
2025-07-23 16:18:15 +0200

    Support adding kids
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- starkids/script.js
+++ starkids/script.js
@@ -23,8 +23,35 @@ function updateName(kid) {
     nameInput.value = name;
 }
 
-for (let i = 1; i <= 3; i++) {
-    const kid = `kid${i}`;
-    updateStars(kid, 0);
-    updateName(kid);
+function createKid(id) {
+    const kidId = `kid${id}`;
+    const kidElement = document.createElement('div');
+    kidElement.className = 'kid';
+    kidElement.id = kidId;
+
+    kidElement.innerHTML = `
+        <input class="name-input" type="text" placeholder="Child ${id}" onchange="updateName('${kidId}')">
+        <div class="controls">
+            <button onclick="updateStars('${kidId}', -1)">☠️</button>
+            <span class="stars">0</span>
+            <button onclick="updateStars('${kidId}', 1)">⭐</button>
+        </div>
+    `;
+
+    document.querySelector('.kids').appendChild(kidElement);
+    updateName(kidId);
+    updateStars(kidId, 0);
 }
+
+let kidCount = parseInt(localStorage.getItem('kid_count')) || 0;
+
+function add() {
+    createKid(++kidCount);
+    localStorage.setItem('kid_count', kidCount);
+}
+
+for (let i = 1; i <= kidCount; i++) {
+    createKid(i);
+}
+
+document.getElementById('add').addEventListener('click', add);
