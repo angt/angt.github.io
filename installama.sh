2aeb0279974ae3ea75becf153227e14967bab15b
Adrien Gallouët <adrien@gallouet.fr>
2025-09-23 18:03:59 +0000

    Update installama.sh
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- installama.sh
+++ installama.sh
@@ -60,35 +60,8 @@ main() {
 	(*) die "OS not supported"
 	esac
 
-	MODEL="${1%%:*}"
-	# MODEL_REF=main
-
-	case "$MODEL" in (*/*) ;;
-	(qwen3-4b)    MODEL="unsloth/Qwen3-4B-Instruct-2507-GGUF" ;;
-	(gpt-oss-20b) MODEL="unsloth/gpt-oss-20b-GGUF" ;;
-	(*) die "Please choose a model, like qwen3-4b:Q4_0"
-	esac
-
-	case "$1" in
-	(*:*) MODEL_QUANT="${1##*:}" ;;
-	(*) die "Please choose a quant type, like $MODEL:Q8_0"
-	esac
-
-	shift 1
 	mkdir -p ~/.installama
 	cd ~/.installama || exit 1
-
-	check_bin hf
-	echo "Downloading model files... this might take a while"
-	MODEL_DIR=$(hf download "$MODEL" --include "*$MODEL_QUANT*" 2>/dev/null)
-
-	[ -d "$MODEL_DIR" ] ||
-		die "Model $MODEL not found"
-
-	MODEL_FILE=$(find "$MODEL_DIR" -name "*$MODEL_QUANT*.gguf" | sort | head -n 1)
-
-	[ -f "$MODEL_FILE" ] ||
-		die "Unable to find the GGUF file in $MODEL_DIR"
 
 	if [ "$OS" = macos ]; then
 		[ -x llama-server ] || llama_server_metal
@@ -101,7 +74,7 @@ main() {
 		"No prebuilt llama-server binary is available for your system." \
 		"Please compile llama.cpp from source instead."
 
-	exec ./llama-server -m "$MODEL_FILE" "$@"
+	exec ./llama-server "$@"
 }
 
 main "$@"
